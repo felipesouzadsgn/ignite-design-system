4f2d8f59e3e3f5ccc949bb9c6ecff1022eab6299
Felipe Souza <felipesouzadsgn@gmail.com>
2023-03-16 13:05:20 -0300

    Adding stories to Text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,45 @@
+import type { StoryObj, Meta } from "@storybook/react";
+import { Text, TextProps } from "@ignite-ui/react";
+
+export default {
+  title: "Form/Text",
+  component: Text,
+  args: {
+    size: "md",
+    children:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, voluptates autem. Iusto odit eligendi porro rerum quas temporibus ipsam sunt. Doloribus qui quos, autem error velit est explicabo delectus tempora!",
+  },
+
+  argTypes: {
+    size: {
+      options: [
+        "xxs",
+        "xs",
+        "sm",
+        "md",
+        "lg",
+        "xl",
+        "2xl",
+        "4xl",
+        "5xl",
+        "6xl",
+        "7xl",
+        "8xl",
+        "9xl",
+      ],
+
+      control: {
+        type: "inline-radio",
+      },
+    },
+  },
+} as Meta<TextProps>;
+
+export const Primary: StoryObj<TextProps> = {};
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: "Strong text",
+    as: "strong",
+  },
+};
